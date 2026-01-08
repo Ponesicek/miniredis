31710245aac57362486c6a197786755f86e971da
ponesicek <62983557+Ponesicek@users.noreply.github.com>
2026-01-08 10:55:57 +0100

    Hashmap doubling

--- hashmap.hpp
+++ hashmap.hpp
@@ -1,11 +1,12 @@
 #include <string>
 #include "linkedlist.hpp"
-#define HASHMAP_SIZE 256
+#include <iostream>
+#define HASHMAP_SIZE 2
 
 class hashmap
 {
 private:
-    LinkedList* list[HASHMAP_SIZE];
+    LinkedList** list;
     int size = HASHMAP_SIZE;
     int ammountOfObjects = 0;
 
@@ -24,6 +25,10 @@ public:
             list[index]->append(key, value);
         }
         ammountOfObjects++;
+        if (ammountOfObjects / size > 2)
+        {
+            doubleSize();
+        }
     }
     std::string get(std::string key)
     {
@@ -35,6 +40,7 @@ public:
         Node* current = list[index]->head;
         while (current)
         {
+            std::cout << "Checking key: " << current->key << std::endl;
             if (current->key == key)
             {
                 return current->value;
@@ -43,10 +49,37 @@ public:
         }
         return "";
     }
+    void doubleSize()
+    {
+        LinkedList** oldList = list;
+        int oldSize = size;
+        size *= 2;
+        list = new LinkedList * [size];
+        for (int i = 0; i < size; i++)
+        {
+            list[i] = nullptr;
+        }
+        for (int i = 0; i < oldSize; i++)
+        {
+            if (oldList[i] != nullptr)
+            {
+                Node* current = oldList[i]->head;
+                while (current)
+                {
+                    insert(current->key, current->value);
+                    current = current->next;
+                }
+                delete oldList[i];
+            }
+        }
+        delete[] oldList;
+        std::cout << "Hashmap size doubled to " << size << std::endl;
+    }
 };
 
 hashmap::hashmap()
 {
+    list = new LinkedList*[HASHMAP_SIZE];
     for (int i = 0; i < HASHMAP_SIZE; i++)
     {
         list[i] = nullptr;
@@ -55,4 +88,9 @@ hashmap::hashmap()
 
 hashmap::~hashmap()
 {
+    for (int i = 0; i < size; i++)
+    {
+        delete list[i];
+    }
+    delete[] list;
 }
--- tests.cpp
+++ tests.cpp
@@ -5,6 +5,11 @@ int main() {
     hashmap map;
     map.insert("name", "Alice");
     map.insert("age", "30");
-    std::cout << map.get("name") << std::endl;
+    map.insert("sex", "female");
+    map.insert("husband", "Peter");
+    map.insert("weight", "70kg");
+    map.insert("pay", "100k");
+    map.insert("height", "160cm");
+    std::cout << map.get("sex") << std::endl;
     return 0;
 }
